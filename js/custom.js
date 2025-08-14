787ea118e0b68fdd53e5a47e2b6814ace2522354
madhavi237 <madhavi@morphicitsolutions.com>
2025-08-14 13:02:47 +0530

    completed homepage design

--- js/custom.js
+++ js/custom.js
@@ -83,3 +83,73 @@ if (document.querySelector('.video-container') != null) {
         });
     });
 }
+
+// Add jquery to before after image slider
+jQuery(function ($) {
+    let before = $('.slider-bef');
+    let handle = $('.handle-wrapper');
+    let slider = $('.before-after-img');
+    let tooltip = $('.slider-tip');
+
+    let sliderWidth = slider.width();
+    let startClip = sliderWidth * 2 / 3; // Initial clip position
+
+    // Set initial clip for before img
+    before.css('clip-path', `inset(0 ${sliderWidth - startClip}px 0 0)`);
+    handle.css('left', startClip + 'px');
+
+    function updateClip() {
+        let handleLeft = parseInt(handle.css('left'), 10);
+        let rightClip = sliderWidth - handleLeft;
+        before.css('clip-path', `inset(0 ${rightClip}px 0 0)`);
+    }
+
+    // Make the handle draggable
+    handle.draggable({
+        containment: slider,
+        axis: 'x',
+        drag: updateClip
+    });
+
+    // Hide tooltip on click
+    slider.on('mousedown', function () {
+        tooltip.fadeOut(10);
+    });
+});
+
+
+// Add read-more and less text
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.read-more-link').forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            let reviewText = this.previousElementSibling;
+            reviewText.classList.toggle('expanded');
+            this.textContent = reviewText.classList.contains('expanded')
+                ? 'Read Less...'
+                : 'Read More...';
+        });
+    });
+});
+
+
+// ******************** Add Main slider js ******************* //
+
+const swiper = new Swiper('#MainSlider', {
+    slidesPerView: 1,
+    speed: 500,
+    loop: true,
+    // autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false
+    // },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '.custom-button-next',
+        prevEl: '.custom-button-prev'
+    }
+});
